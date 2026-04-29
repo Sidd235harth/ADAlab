26c5b8513de8d2b6475ead70b00f1ad5fdb942e3
Siddharth TR <siddartha.24.beai@acharya.ac.in>
2026-04-29 15:56:28 +0530

    Create ADAlab3.c

---
+++ ADAlab3.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void merge(int a[], int l, int m, int r) {
+    int i = l, j = m + 1, k = 0;
+    int *t = (int *)malloc((r - l + 1) * sizeof(int));
+
+    while (i <= m && j <= r)
+        t[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
+
+    while (i <= m)
+        t[k++] = a[i++];
+
+    while (j <= r)
+        t[k++] = a[j++];
+
+    for (i = l, k = 0; i <= r; i++, k++)
+        a[i] = t[k];
+
+    free(t);
+}
+
+void mergesort(int a[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        mergesort(a, l, m);
+        mergesort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+
+void printArray(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of Elements: ");
+    scanf("%d", &n);
+
+    int *a = (int *)malloc(n * sizeof(int));
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 1000;
+
+    printf("\nArray before sorting:\n");
+    printArray(a, n);
+
+    clock_t s = clock();
+
+    mergesort(a, 0, n - 1);
+
+    clock_t e = clock();
+
+    printf("\nArray after sorting:\n");
+    printArray(a, n);
+
+    printf("\nTime = %lf seconds\n",
+           (double)(e - s) / CLOCKS_PER_SEC);
+
+    free(a);
+    return 0;
+}
